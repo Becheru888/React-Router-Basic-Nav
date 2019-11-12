ce4f30d1e9395d30810b7a94e3d2f1b4df6cebe8
Becheru888 <becheru93@gmail.com>
2019-11-12 12:59:58 +0000

    pr fixed

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
-    <Navigation />
+    <Navigation/>
     <Route exact path={'/'} component={Home}/>
-    <Route exact path={'/about'} component={about}/>
-    <Route exact path={'/Contact'} component={Contact}/>
+    <Route exact path={'/about'} component={About}/>
+    <Route exact path={'/contact'} component={Contact}/>
   </div>
 );
 
